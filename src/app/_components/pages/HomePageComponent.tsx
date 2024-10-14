170462214150bf4719337903d47bb27b273fb481
washizawa haruya (JSL) <129074090+washizawa83@users.noreply.github.com>
2024-10-14 12:01:08 +0900

    利用規約・プライバシーポリシー追加 (#4)

--- src/app/_components/pages/HomePageComponent.tsx
+++ src/app/_components/pages/HomePageComponent.tsx
@@ -4,6 +4,7 @@ import { BasePage } from '@/app/_components/layouts/BasePage'
 import { OrbEffect } from '@/app/_components/layouts/OrbEffect'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import Link from 'next/link'
 import logoUrl from '../../../../public/typeranker.svg'
 
 export const HomePageComponent = () => {
@@ -24,6 +25,15 @@ export const HomePageComponent = () => {
                     <HomeMenuButton label="training" path="training" />
                     <HomeMenuButton label="performance" path="performance" />
                 </div>
+                <footer className="absolute bottom-5 left-0 m-auto flex w-96 flex-col text-center text-sm">
+                    <div className="flex justify-around bg-[#181716d9]">
+                        <Link href="/pages/policy/terms-of-use">利用規約</Link>
+                        <Link href="/pages/policy/privacy-policy">
+                            プライバシーポリシー
+                        </Link>
+                        <p>&copy; 2024 Typerancker</p>
+                    </div>
+                </footer>
             </BasePage>
         </div>
     )
